e1b4f5559581a724fbb42f63a50c11195f03ec8f
Fikhan <abdul.adziq.fikhan@gmail.com>
2023-09-26 16:30:17 +0700

    menambahkan control Contoh1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya abdul abdul adziq fikhan
+            saya tinggal di daerah
+            olah raga yang saya sukai adalah voli";
+    }
+}
