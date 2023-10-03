d863d9a38e0c72a6ae3569b77a399f9b95ddc787
Fikhan <abdul.adziq.fikhan@gmail.com>
2023-10-03 21:16:55 +0700

    Latihan1

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -4,7 +4,13 @@ class Latihan1 extends CI_Controller
     public fuction index()
     {
         echo "Selamat Datang.. selamat belajar web Programming"
-
+        //$this->load->view('view-Latihan1');
     }
-    
+
+    public function penjumlahan($n1, $n2)
+    {
+        $this->load->model('Model_Latihan1');
+        $hasil = $this->Model_Latihan1->jumlah($n1, $n2);
+        echo "Hasil Penjumlahan dari". $n1 ." + ". $n2 ." + ".$hasil;
+    }
 }
