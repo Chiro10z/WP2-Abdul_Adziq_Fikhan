cf5b479592cc31b938d28499d0a11b663d316245
Fikhan <abdul.adziq.fikhan@gmail.com>
2023-10-03 22:16:35 +0700

    Latihan1

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -10,8 +10,11 @@ class Latihan1 extends CI_Controller
     public function penjumlahan($n1, $n2)
     {
         $this->load->model('Model_Latihan1');
+
+        $data['nilai1'] = $n1;
+        $data['nilai2'] = $n2;
         $hasil = $this->Model_Latihan1->jumlah($n1, $n2);
-        echo "Hasil Penjumlahan dari". $n1 ." + ". $n2 ." = "
-.$hasil;
+        
+        $this->load->view('view-latihan1', $data);
     }
 }
